1e719248a9b3647070f66b66fa0114a46f347589
Markus Aleksander Råkil Johansen <markusrj@stud.ntnu.no>
2022-03-02 09:19:50 +0100

    Tried to add icon.png to app, without sucess

--- src/main/java/BookOfCook/BookOfCookApp.java
+++ src/main/java/BookOfCook/BookOfCookApp.java
@@ -2,13 +2,15 @@ package BookOfCook;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class BookOfCookApp extends Application{
 
-    @Override
+    @Override   
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("Book of Cook");
+        primaryStage.getIcons().add(new Image("file:icon.png")); //!funker ikke :()
         primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("BookOfCook.fxml"))));
         primaryStage.show();        
     }
