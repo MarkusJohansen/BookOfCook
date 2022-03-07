0f028d78b363fda226ed0acd24cc6375a0b2e195
Markus Aleksander Råkil Johansen <markusrj@stud.ntnu.no>
2022-03-07 13:03:42 +0100

    ryddet opp i recipe dokumentet

--- src/main/java/BookOfCook/Recipe.java
+++ src/main/java/BookOfCook/Recipe.java
@@ -1,76 +1,23 @@
-/*
-TODO: Possible scenarios:
-- Forskjellige capitalisering av samme ingrediens
-- legge til samme ingrediens to ganger
-- negativt antall calorier, servings
-- Illegal chars in Recipe name
-- Illegal chars in Ingredient name
-- Kun tall i ingrdent amount
-- ikke tall i units
-
-?MULIGE LØSNINGER:
-- ikke skille mellom Tomat, tomat og tOmAt
-- solve issue with illegal symbols by regex
-*/
-
 package BookOfCook;
 
 import java.util.*;
 
 public class Recipe {
-    private String name, displayedName;                                                                       //name of recipe
+    private String name, displayedName;                                                                     //name of recipe that will be dealt with back end, as well as name displayed to user    
     private int numberOfServings;                                                                           //number persons this recipe serves        
     private double calories, caloriesPerPerson;                                                             //calories in recipe, calories per person                                                                 
     private ArrayList<HashMap<String,Object>> Ingredients = new ArrayList<HashMap<String,Object>>();        //uses <String, Object> to store the ingredient name-, amount- and unit-strings, but at the same time be able to set the key equal to differnt datatypes
     private ArrayList<Category> categories = new ArrayList<Category>();                                     //stores the categories of the recipe                                    
     private ArrayList<String> steps = new ArrayList<String>();                                              //stores the steps of how to make the recipe
 
-    //*GENERAL TOOLBOX METHODS
-    private String capitalize(String s) {
-        return s.substring(0,1).toUpperCase() + s.substring(1).toLowerCase();             //capitalizes first letter of string s and returns it
-    }
-
-    private void whiteSpaceCheck(String s){
-        //check if string starts or ends with multiple white spaces
-        if (s.startsWith(" ") || s.endsWith(" ")) {
-            throw new IllegalArgumentException("String starts or ends with white spaces");
-        }
-    }
-
     //*CONSTRUCTOR                                                                                          
     public Recipe(String name, int numberOfServings) {                      //constructor for recipe demands that recipe has a defined name and number of servings
-        validateConstructor(name, numberOfServings);
-
-        this.name = name.toUpperCase();                                     //sets name to upper case
-        this.displayedName = capitalize(name);                                //sets displayname of recipe
-        this.numberOfServings = numberOfServings;                           //sets number of servings so we can do logical operations linked to scaling of the recipe
+        setName(name);
+        setNumberOfServings(numberOfServings);
     }
 
-    //*RECIPE VALIDATION METHODS
-    public void validateConstructor(String name, int numberOfServings) {     //validates the constructor
-        validRecipeName(name);
-        validServings(numberOfServings);
-    }
-
-    public void validRecipeName(String name) {
-        if (name == null || name.equals("")) {                          //if name is null or empty
-            throw new IllegalArgumentException("Name cannot be null or empty");
-        }
-        if (!(name.matches("[a-zA-Z\\- ]+"))) { 
-            throw new IllegalArgumentException("Name cannot contain numbers or special characters");
-        }
-        whiteSpaceCheck(name);
-    }
-
-    public void validServings(int numberOfServings) {
-        if (numberOfServings <= 0) {                                    //if number of servings is less than or equal to 0
-            throw new IllegalArgumentException("Number of servings must be greater than 0");
-        }
-        if (numberOfServings > 100) {                                   //! skal vi sette constraint? if number of servings is greater than 100
-            throw new IllegalArgumentException("Number of servings must be less than 100");
-        }
-    }
-
+    
+    //!SJEKK HVEM SOM FAKTISK BRUKES
     //*GETTERS               
     //get name of recipe   
     public String getName() {
@@ -107,7 +54,8 @@ public class Recipe {
         return steps;                                                      //returns steps
     }
 
-    //*CHANGING NAME and servings
+
+    //*SETTERS
     //change name of recipe
     public void setName(String name) {
         validRecipeName(name);
@@ -121,7 +69,20 @@ public class Recipe {
         this.numberOfServings = numberOfServings;                             //sets number of servings
     }
 
-    //*ADDING, REMOVING AND INGREDIENTS
+    //set calories
+    public void setCalories(double calories) {   
+        validateCalories(calories);                       
+        this.calories = calories;                                           //sets the total calories of the recipe
+        setCaloriesPerPerson();                                             //sets calories per person based on the new value of calories and number of servings
+    }
+
+    //!set calories per person should be combined with setCalories
+    private void setCaloriesPerPerson() {          
+        this.caloriesPerPerson = calories / numberOfServings;               //sets calories per person based on the new value of calories and number of servings
+    }
+
+
+    //*CHANGE AND MANIPULATE DATA
     //add ingredient to list of ingredients in recipe
     public void addIngredient(String name, double amount, String unit) {    
         HashMap<String,Object> ingredient = new HashMap<String,Object>();   //Creates a hasmap called ingredient that stores different properties of the ingredient
@@ -135,7 +96,7 @@ public class Recipe {
         Ingredients.add(ingredient);                                        //adds the ingredient to the list of ingredients
     }
 
-    // remove ingredient
+    //remove ingredient
     public void removeIngredient(String name) {                 
         for (HashMap<String, Object> Ingredient : Ingredients) {            //loops through all ingredients
             if (Ingredient.get("name").equals(name.toUpperCase())) {        //looks for a ingredient that mathces the name of the element to be removed
@@ -145,7 +106,7 @@ public class Recipe {
         }
     }
 
-    //change ingredient in ingredients list
+    //change ingredient amount and unit in ingredients list
     public void changeIngredient(String name, double amount, String unit) {
         for (HashMap<String, Object> Ingredient : Ingredients) {            //loops through all ingredients
             HashMap<String, Object> copyOfIngredient = Ingredient;          //creates a copy of the ingredient
@@ -188,58 +149,40 @@ public class Recipe {
         }
     }
 
-    //*VALIDATION OF INGREDIENTS
-    private void validateIngredient(HashMap<String,Object> Ingredient) {
-        nullValueCheck(Ingredient);        
-        regexCheck(Ingredient);
-        whiteSpaceCheck(Ingredient.get("name").toString());
+    //!needs validation
+    //establishes a relation between a category and a recipe
+    public void addCategory(Category category) { 
+        if(categories.contains(category)){                                  //if the category already exists in the recipe
+            return;                                                         //returns to avoid adding the category again
+        }          
+        categories.add(category);                                           //adds category to list of categorys in recipe
+        category.addRecipe(this);                                           //adds recipe to list of recipes in category. establishes the n to n relationship between category and recipe
     }
 
-    private void nullValueCheck(HashMap<String,Object> Ingredient) {
-        //check if values of ingredient are null
-        if (Ingredient.get("name") == null || Ingredient.get("amount") == null || Ingredient.get("unit") == null) {
-            throw new IllegalArgumentException("One or more values of the ingredient is null.");
+    //!needs validation
+    //removes relation between a category and a recipe
+    public void removeCategory(Category category) {
+        if(!categories.contains(category)){                                 //if the category does not exist in the recipe
+            return;                                                         //returns to avoid removing non existing fcategory                 
         }
+        categories.remove(category);                                        //removes category from list of categories in recipe
+        category.removeRecipe(this);                                        //removes recipe from list of recipes in category. 
     }
 
-    private void regexCheck(HashMap<String,Object> Ingredient) {
-        if (!(Ingredient.get("amount").toString().matches("[0-9]*\\.?[0-9]*") || Ingredient.get("amount").toString().matches("[0-9]*"))) {
-            throw new IllegalArgumentException("amount is not a Double or integer");
-        }
-        if (!(Ingredient.get("unit").toString().matches("[a-z]+"))) {
-            throw new IllegalArgumentException("unit contains invalid characters");
-        }
-        if (!(Ingredient.get("name").toString().matches("[A-Z\\- ]+") || Ingredient.get("displayName").toString().matches("[a-zA-Z\\- ]+"))) {
-            throw new IllegalArgumentException("name or displayName contains invalid characters");
-        }
+    //add step to recipe steps
+    public void addStep(String step) {
+        validateStep(capitalize(step));                                            
+        steps.add(capitalize(step));                                                    //adds step to list of steps in recipe
     }
 
-    //*SETTING AND CHANGING CALORIES
-    //set calories
-    public void setCalories(double calories) {   
-        validateCalories(calories);                       
-        this.calories = calories;                                           //sets the total calories of the recipe
-        setCaloriesPerPerson();                                             //sets calories per person based on the new value of calories and number of servings
+    //remove step from recipe steps by index
+    public void removeStep(int step) {
+        validateStepIndex(step);                                                          
+        steps.remove(step);                                                //removes step from list of steps in recipe
     }
 
-    //set calories per person
-    private void setCaloriesPerPerson() {          
-        this.caloriesPerPerson = calories / numberOfServings;               //sets calories per person based on the new value of calories and number of servings
-    }
 
-    //*VALIDATION OF CALORIES
-    private void validateCalories(double calories) {
-        if (calories < 0) {
-            throw new IllegalArgumentException("calories cannot be negative");
-        }
-    }
-
-    //*SCALING AND CHANGING SERVINGS
-    //change number of servings
-    public void changeNumberOfServings(int newNumberOfServings) {   
-        numberOfServings = newNumberOfServings;                             //changes number of servings to new number of servings
-    }
-
+    //*SCALE RECIPE
     //when numbers of servings change, scale amounts
     public void scaleRecipe(int newNumberOfServings) { 
         validServings(newNumberOfServings);                                  //validates new number of servings¨
@@ -256,42 +199,8 @@ public class Recipe {
         setCaloriesPerPerson();                                            //sets calories per person based on the new value of number of servings
     }
 
-    //!VALIDATIONS missing
-    //*CATEGORY METHODS
-    //establishes a relation between a category and a recipe
-    public void addCategory(Category category) { 
-        if(categories.contains(category)){                                  //if the category already exists in the recipe
-            return;                                                         //returns to avoid adding the category again
-        }          
-        categories.add(category);                                           //adds category to list of categorys in recipe
-        category.addRecipe(this);                                           //adds recipe to list of recipes in category. establishes the n to n relationship between category and recipe
-    }
 
-
-    //removes relation between a category and a recipe
-    public void removeCategory(Category category) {
-        if(!categories.contains(category)){                                 //if the category does not exist in the recipe
-            return;                                                         //returns to avoid removing non existing fcategory                 
-        }
-        categories.remove(category);                                        //removes category from list of categories in recipe
-        category.removeRecipe(this);                                        //removes recipe from list of recipes in category. 
-    }
-
-
-    //*STEP METHODS
-    //add step to recipe steps
-    public void addStep(String step) {
-        validateStep(capitalize(step));                                            //!validates step but has wierd name
-        steps.add(capitalize(step));                                                    //adds step to list of steps in recipe
-    }
-
-    //remove step from recipe steps by index
-    public void removeStep(int step) {
-        validateStepIndex(step);                                                          //!validates step but has wierd name
-        steps.remove(step);                                                //removes step from list of steps in recipe
-    }
-
-    //*STEP VALIDATIONS
+    //*VALIDATIONS
     //validates step index
     private void validateStepIndex(int step) {
         if (step < 0 || step > steps.size()) {
@@ -307,8 +216,64 @@ public class Recipe {
         whiteSpaceCheck(name);
     }
 
+    //validates Calories
+    private void validateCalories(double calories) {
+        if (calories < 0) {
+            throw new IllegalArgumentException("calories cannot be negative");
+        }
+    }
+
+    //validates Ingredient
+    private void validateIngredient(HashMap<String,Object> Ingredient) {
+        nullValueCheck(Ingredient);        
+        regexCheck(Ingredient);
+        whiteSpaceCheck(Ingredient.get("name").toString());
+    }
+
+    //checks if values in ingredient is null
+    private void nullValueCheck(HashMap<String,Object> Ingredient) {
+        //check if values of ingredient are null
+        if (Ingredient.get("name") == null || Ingredient.get("amount") == null || Ingredient.get("unit") == null) {
+            throw new IllegalArgumentException("One or more values of the ingredient is null.");
+        }
+    }
+
+    //checks if values in ingredient are valid according to regex patterns
+    private void regexCheck(HashMap<String,Object> Ingredient) {
+        if (!(Ingredient.get("amount").toString().matches("[0-9]*\\.?[0-9]*") || Ingredient.get("amount").toString().matches("[0-9]*"))) {
+            throw new IllegalArgumentException("amount is not a Double or integer");
+        }
+        if (!(Ingredient.get("unit").toString().matches("[a-z]+"))) {
+            throw new IllegalArgumentException("unit contains invalid characters");
+        }
+        if (!(Ingredient.get("name").toString().matches("[A-Z\\- ]+") || Ingredient.get("displayName").toString().matches("[a-zA-Z\\- ]+"))) {
+            throw new IllegalArgumentException("name or displayName contains invalid characters");
+        }
+    }
+
+    //validates recipe name
+    public void validRecipeName(String name) {
+        if (name == null || name.equals("")) {                          //if name is null or empty
+            throw new IllegalArgumentException("Name cannot be null or empty");
+        }
+        if (!(name.matches("[a-zA-Z\\- ]+"))) { 
+            throw new IllegalArgumentException("Name cannot contain numbers or special characters");
+        }
+        whiteSpaceCheck(name);
+    }
+
+    //validates number of servings
+    public void validServings(int numberOfServings) {
+        if (numberOfServings <= 0) {                                    //if number of servings is less than or equal to 0
+            throw new IllegalArgumentException("Number of servings must be greater than 0");
+        }
+        if (numberOfServings > 100) {                                   //! skal vi sette constraint? if number of servings is greater than 100
+            throw new IllegalArgumentException("Number of servings must be less than 100");
+        }
+    }
+
+
     //*PARSING RECIPE FOR WRITING TO .txt FILE
-    //writes parsed string that can be written to txt file
     public String parsedRecipe() {
         String parsedRecipe = "";                                                               //Creates empty string that will be built up
         String recipeSection = "============================================\n";                //Creates a FATLINE to separate different sections.
@@ -343,6 +308,20 @@ public class Recipe {
     }
 
 
+    //*GENERAL TOOLBOX METHODS
+    //capitalizes first letter and makes rest of string lowercase. good for displayed names in app
+    private String capitalize(String s) {
+        return s.substring(0,1).toUpperCase() + s.substring(1).toLowerCase();             //capitalizes first letter of string s and returns it
+    }
+
+    //check if string starts or ends with multiple white spaces
+    private void whiteSpaceCheck(String s){
+        if (s.startsWith(" ") || s.endsWith(" ")) {
+            throw new IllegalArgumentException("String starts or ends with white spaces");
+        }
+    }
+
+
     //*TOSTRING METHOD
     @Override
     public String toString() {
@@ -362,7 +341,6 @@ public class Recipe {
         Category italian = new Category("Italian");
         Category dessert = new Category("Dessert");
 
-        
         //adds base ingredients and calories to recipe
         recipe.setCalories(1000);
         recipe.addIngredient("tomater", 100, "gram");
@@ -371,7 +349,6 @@ public class Recipe {
         recipe.addIngredient("mel", 100 , "kg");
         recipe.addIngredient("pizza kulsyre", 1, "stk");
         System.out.println(recipe.parsedRecipe());
-
 
         //testing av skalering og endring av ingrdiens
         recipe.scaleRecipe(4);
@@ -386,7 +363,6 @@ public class Recipe {
         recipe.addCategory(dessert);
         recipe.removeCategory(dessert);
         System.out.println(recipe.parsedRecipe());
-
 
         //legger til steg in recipe
         recipe.addStep("Putt tomater i en kasse");
