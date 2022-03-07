f7ae140acad1e182f01d3d66c6c28d83e7f038be
Julian <julianao@stud.ntnu.no>
2022-03-07 16:14:33 +0100

    la til sortering etter kategorier i cookbook.java

--- src/main/java/BookOfCook/Cookbook.java
+++ src/main/java/BookOfCook/Cookbook.java
@@ -12,23 +12,22 @@ public class Cookbook {
     // *CONSTRUCTOR
     // constructor for cookbook demands name and is always empty at creation
     public Cookbook(String name) {
-        this.name = name; // name of cookbook
-        this.recipeAmount = 0; // amount of recipes in cookbook
+        this.name = name;           // name of cookbook
+        this.recipeAmount = 0;      // amount of recipes in cookbook
     }
 
     // *RECIPE ADD AND REMOVE METHODS
     // adds recipe to cookbook
     public void addRecipeToCookbook(Recipe recipe) {
-        if (recipes.contains(recipe)) { // checks if recipe already exists in cookbook
-            System.out.println("Recipe already exists in cookbook"); // describes problem in console
-            return; // return to prevent adding already existing recipe again
+        if (recipes.contains(recipe)) {                                 // checks if recipe already exists in cookbook
+            System.out.println("Recipe already exists in cookbook");    // describes problem in console
+            return;                                                     // return to prevent adding already existing recipe again
         }
 
-        for (Recipe r : recipes) { // loops through recipes in cookbook
-            if (r.getName().equals(recipe.getName())) { // checks there is another recipe with identical name in
-                                                        // cookbook
+        for (Recipe r : recipes) {                          // loops through recipes in cookbook
+            if (r.getName().equals(recipe.getName())) {     // checks there is another recipe with identical name in cookbook
                 System.out.println("Recipe with same name already exists in cookbook"); // describes problem in console
-                return; // return to prevent adding recipe with same name again
+                return;                                     // return to prevent adding recipe with same name again
             }
         }
 
@@ -65,20 +64,36 @@ public class Cookbook {
     // write recipes in cookbook as parsed strings to file
     public void writeToFile() {
         try { // prøv å skrive filen
-            FileWriter fileWriter = new FileWriter(name + ".txt"); // lager et filskriver objekt som spesifiserer at
-                                                                   // filen skal hete "navnet til cookbook".txt
+            FileWriter fileWriter = new FileWriter(name + ".txt"); // lager et filskriver objekt som spesifiserer at filen skal hete "navnet til cookbook".txt
 
-            for (Recipe recipe : recipes) { // looper gjennom alle matoppskrifter i cookbook
-                fileWriter.write(recipe.parsedRecipe()); // skriver matoppskriften til filen som en parset string
-                                                         // gjennom parsedRecipe() metoden
+            for (Recipe recipe : recipes) {                 // looper gjennom alle matoppskrifter i cookbook
+                fileWriter.write(recipe.parsedRecipe());    // skriver matoppskriften til filen som en parset string gjennom parsedRecipe() metoden
             }
 
-            fileWriter.close(); // lukker skriveren og indikerer at filen er ferdigskrevet.
+            fileWriter.close();             // lukker skriveren og indikerer at filen er ferdigskrevet.
         }
 
-        catch (IOException exception) { // dersom det oppstår en feil ved skriving av filen fanger den opp feilen
-            exception.printStackTrace(); // skriver ut feilen til konsollen
+        catch (IOException exception) {     // dersom det oppstår en feil ved skriving av filen fanger den opp feilen
+            exception.printStackTrace();    // skriver ut feilen til konsollen
         }
+    }
+
+    // *GET RECIPES IN CATEGORIES METHODS
+
+    public ArrayList<Recipe> getSortedRecipes(ArrayList<Category> categories){  // categories as parameter
+
+        ArrayList<Recipe> sortedRecipes = new ArrayList<>();     // create an output arraylist
+
+        for (Recipe recipe : recipes) {                             // loops through all recipes in cookbook
+            for (Category category : categories) {                  // loops through all categories
+                if(recipe.getCategories().contains(category)){      // checks if recipe has this category
+
+                    sortedRecipes.add(recipe);                   // if true: add recipe to sortedRecipes
+                }
+            }
+        }
+
+        return sortedRecipes;
     }
 
     // *TOSTRING METHOD
@@ -93,7 +108,7 @@ public class Cookbook {
 
     // *MAIN METHOD
     public static void main(String[] args) {
-        Cookbook cookbook = new Cookbook("Cookbook");
+        /* Cookbook cookbook = new Cookbook("Cookbook");
         Recipe recipe1 = new Recipe("Recipe1", 2);
         Recipe recipe2 = new Recipe("Recipe2", 4);
         Recipe recipe3 = new Recipe("Recipe3", 1);
@@ -102,6 +117,33 @@ public class Cookbook {
         cookbook.addRecipeToCookbook(recipe3);
         System.out.println(cookbook);
 
-        cookbook.writeToFile();
+        cookbook.writeToFile(); */
+
+
+        Cookbook cookbook = new Cookbook("Cookbook");
+        Recipe pasta_carbonara = new Recipe("pasta carbonara", 2);
+        Recipe pasta_bolognese = new Recipe("pasta bolognese", 4);
+        Recipe cheeseburger = new Recipe("cheeseburger", 1);
+        Recipe vegetarburger = new Recipe("vegetarburger", 1);
+
+        Category italiensk = new Category("italiensk");
+        Category burger = new Category("burger");
+        Category vegetar = new Category("vegetar");
+
+        pasta_carbonara.addCategory(italiensk);
+        pasta_bolognese.addCategory(italiensk);
+        cheeseburger.addCategory(burger);
+        vegetarburger.addCategory(burger);
+        vegetarburger.addCategory(vegetar);
+
+        cookbook.addRecipeToCookbook(pasta_carbonara);
+        cookbook.addRecipeToCookbook(pasta_bolognese);
+        cookbook.addRecipeToCookbook(cheeseburger);
+        cookbook.addRecipeToCookbook(vegetarburger);
+
+        ArrayList<Category> categories = new ArrayList<>(Arrays.asList(italiensk, vegetar)); // lager en arraylist med kategorier jeg vil sortere etter
+
+        System.out.println(cookbook.getSortedRecipes(categories));
+        
     }
 }
